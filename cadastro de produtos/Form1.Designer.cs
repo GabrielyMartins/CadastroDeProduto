0f11d4ec0c4cc7d2bc85008547bf5f3d2c6cdc50
Gabriely Maritns <gabrielybudim@gmail.com>
2023-08-01 15:30:26 -0400

    primerio commit

---
+++ cadastro de produtos/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace cadastro_de_produtos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx_codigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tx_descricao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tx_aquisicao = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tx_lucro = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tx_venda = new System.Windows.Forms.TextBox();
+            this.bt_novo = new System.Windows.Forms.Button();
+            this.bt_cadastrar = new System.Windows.Forms.Button();
+            this.bt_cancelar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tx_codigo
+            // 
+            this.tx_codigo.Location = new System.Drawing.Point(209, 79);
+            this.tx_codigo.Name = "tx_codigo";
+            this.tx_codigo.Size = new System.Drawing.Size(100, 20);
+            this.tx_codigo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(295, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cadastro de Produto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(143, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Código:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(371, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Descrição:";
+            // 
+            // tx_descricao
+            // 
+            this.tx_descricao.Location = new System.Drawing.Point(457, 78);
+            this.tx_descricao.Name = "tx_descricao";
+            this.tx_descricao.Size = new System.Drawing.Size(100, 20);
+            this.tx_descricao.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(63, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Preço de Aquisição:";
+            // 
+            // tx_aquisicao
+            // 
+            this.tx_aquisicao.Location = new System.Drawing.Point(209, 124);
+            this.tx_aquisicao.Name = "tx_aquisicao";
+            this.tx_aquisicao.Size = new System.Drawing.Size(100, 20);
+            this.tx_aquisicao.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(384, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Lucro %:";
+            // 
+            // tx_lucro
+            // 
+            this.tx_lucro.Location = new System.Drawing.Point(457, 127);
+            this.tx_lucro.Name = "tx_lucro";
+            this.tx_lucro.Size = new System.Drawing.Size(100, 20);
+            this.tx_lucro.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(92, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 18);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Valor da Venda:";
+            // 
+            // tx_venda
+            // 
+            this.tx_venda.Location = new System.Drawing.Point(209, 165);
+            this.tx_venda.Name = "tx_venda";
+            this.tx_venda.Size = new System.Drawing.Size(100, 20);
+            this.tx_venda.TabIndex = 10;
+            // 
+            // bt_novo
+            // 
+            this.bt_novo.Location = new System.Drawing.Point(345, 203);
+            this.bt_novo.Name = "bt_novo";
+            this.bt_novo.Size = new System.Drawing.Size(75, 23);
+            this.bt_novo.TabIndex = 11;
+            this.bt_novo.Text = "Novo";
+            this.bt_novo.UseVisualStyleBackColor = true;
+            // 
+            // bt_cadastrar
+            // 
+            this.bt_cadastrar.Location = new System.Drawing.Point(448, 203);
+            this.bt_cadastrar.Name = "bt_cadastrar";
+            this.bt_cadastrar.Size = new System.Drawing.Size(75, 23);
+            this.bt_cadastrar.TabIndex = 12;
+            this.bt_cadastrar.Text = "Cadastrar";
+            this.bt_cadastrar.UseVisualStyleBackColor = true;
+            // 
+            // bt_cancelar
+            // 
+            this.bt_cancelar.Location = new System.Drawing.Point(558, 203);
+            this.bt_cancelar.Name = "bt_cancelar";
+            this.bt_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.bt_cancelar.TabIndex = 13;
+            this.bt_cancelar.Text = "Cancelar";
+            this.bt_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(146, 260);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(508, 150);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bt_cancelar);
+            this.Controls.Add(this.bt_cadastrar);
+            this.Controls.Add(this.bt_novo);
+            this.Controls.Add(this.tx_venda);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tx_lucro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tx_aquisicao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tx_descricao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tx_codigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx_codigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tx_descricao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tx_aquisicao;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tx_lucro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tx_venda;
+        private System.Windows.Forms.Button bt_novo;
+        private System.Windows.Forms.Button bt_cadastrar;
+        private System.Windows.Forms.Button bt_cancelar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
